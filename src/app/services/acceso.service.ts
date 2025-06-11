70442fe50a378ffd2302cdb0bbd4355da8fae108
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-06-10 19:10:16 -0500

    front2

--- src/app/services/acceso.service.ts
+++ src/app/services/acceso.service.ts
@@ -1,24 +1,27 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { appsettings } from '../settings/appsettings';
 import { User } from '../interface/user';
 import { Observable } from 'rxjs';
 import { ResponseAcceso } from '../interface/responseAcceso';
 import { Login } from '../interface/login';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccesoService {
   private http = inject(HttpClient);
-  private baseUrl = appsettings.apiUrl;
+  private baseUrl = environment.apiUrl;
+
   constructor() {}
+
   registrarse(objeto: User): Observable<ResponseAcceso> {
     return this.http.post<ResponseAcceso>(
       `${this.baseUrl}/registrarse`,
       objeto
     );
   }
+
   login(objeto: Login): Observable<ResponseAcceso> {
     return this.http.post<ResponseAcceso>(`${this.baseUrl}/login`, objeto);
   }
