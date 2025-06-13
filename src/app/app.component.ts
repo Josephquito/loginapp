0434fd0c6f475b3e9c6eae715aa6d224ac6fb0c7
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-06-13 12:25:09 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, computed } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layout/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'front';
+  private router = inject(Router);
+
+  hideSidebar = computed(() => {
+    const hiddenRoutes = ['/login', '/register'];
+    return hiddenRoutes.includes(this.router.url);
+  });
 }
