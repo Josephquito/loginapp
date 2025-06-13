41d0097ca5341ab4d524d292a77fd7a223422f2f
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-06-12 19:00:11 -0500

    auth guard1

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -11,13 +11,18 @@ import { inject, Injectable } from '@angular/core';
 })
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
     const token = window.localStorage.getItem('token');
-    if (!token) return true;
-    this.router.navigate(['/login']);
+
+    if (token) {
+      return true; // ✅ Permitir acceso si hay token
+    }
+
+    this.router.navigate(['/login']); // ❌ Redirigir si NO hay token
     return false;
   }
 }
